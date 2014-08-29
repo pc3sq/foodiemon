f9a2cd31354ce26d32c919c4111f7245e4535c98
Peter Ching <peter.ching@gmail.com>
2014-08-29 17:10:53 -0400

    Add User Authentication

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,5 +8,18 @@ get '/login' do
 end
 
 post '/login' do
+  foody = Foody.find_by(name: params[:foody][:name])
+  if foody && foody.authenticate(params[:foody][:password])
+    puts "Success"
+    session[:id] = foody.id
+    redirect to "/foody/#{session[:id]}"
+  else
+    puts "You suck"
+    # Set errors based on validations
+    redirect to '/login'
+  end
+end
+
+get '/foody/:id' do
   "Hello World"
 end
