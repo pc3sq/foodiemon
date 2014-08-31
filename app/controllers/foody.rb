abef7690af3dc87cb14847221d487d787cc34013
Nick Gio <ngiovacchini@gmail.com>
2014-08-30 23:50:31 -0400

    edit complete, tightening up some redirects

--- app/controllers/foody.rb
+++ app/controllers/foody.rb
@@ -3,6 +3,8 @@ get '/foody/new' do
   erb :"/foody/signup"
 end
 
+### CREATE ###
+
 post '/foodies' do
   foody = Foody.create(params[:foody])
   session[:foody] = foody
@@ -11,12 +13,15 @@ post '/foodies' do
 end
 
 get '/foody/:id' do
-  "Hello World"
+  
+  erb :"/foody/show"
 end
 
 put '/foody/:id' do
   redirect "/foody/#{session[:foody][:id]}"
 end
+
+### UPDATE ###
 
 get '/foody/:id/edit' do
   if params[:id].to_i == session[:foody][:id]
@@ -25,3 +30,20 @@ get '/foody/:id/edit' do
     redirect "/login"
   end
 end
+
+# Could create route to redirect to signup if user is 
+# not found in the database
+
+put '/foodies' do
+  redirect "/foody/#{session[:foody][:id]}"
+end
+
+### DELETE ###
+# Delete account & redirect to signup page
+
+
+
+
+
+
+
